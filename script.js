dc36e9ee3ba92e26c495d65a932919dac41b1630
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-03-21 21:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 function calculateMinCost() {
-  //your code here
-var res = 0;
-	var cost = 0;
-	var inputArray = document.getElementById("rope-lengths").value;
+  var res = 0;
+  var cost = 0;
+  var inputArray = document.getElementById("rope-lengths").value;
 
-	inputArray = inputArray.split(',').map(Number);
-	console.log(inputArray);
-	for(let i = 0;i< inputArray.length -1;i+=0) {
-		if (inputArray.length == 0) break;
-		inputArray = inputArray.sort((a,b) => b-a);
-		res = inputArray.pop()+ inputArray.pop();
-		cost += res;
-		inputArray[inputArray.length] = res; }
+  inputArray = inputArray.split(',').map(Number);
+  console.log(inputArray);
 
-	document.getElementById("result").innerHTML = cost;
-	}
-  
+  for (let i = 0; i < inputArray.length - 1; i += 0) {
+    if (inputArray.length == 0) break;
+    inputArray = inputArray.sort((a, b) => b - a);
+    res = inputArray.pop() + inputArray.pop();
+    cost += res;
+    inputArray[inputArray.length] = res;
+  }
 
-footer
+  document.getElementById("result").innerHTML = cost;
+}
